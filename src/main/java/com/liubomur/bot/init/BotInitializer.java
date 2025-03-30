66f67cb976fdba2ce217ab87a60159aa6896da10
Liubomur <liubomurpryimak@gmail.com>
2025-03-30 19:53:35 +0300

    usage of spring boot env variable to serve BOT_TOKEN;

--- src/main/java/com/liubomur/bot/init/BotInitializer.java
+++ src/main/java/com/liubomur/bot/init/BotInitializer.java
@@ -4,11 +4,10 @@ import com.liubomur.bot.GroomingReminderBot;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-import static com.liubomur.bot.constants.Constants.BOT_TOKEN;
 
 /**
  * This service class serves only one purpose - starting a telegram bot.
@@ -19,6 +18,9 @@ public class BotInitializer {
     private static final Logger logger = LoggerFactory.getLogger(BotInitializer.class);
     private final GroomingReminderBot bot;
 
+    @Value("${bot.token}")
+    private String botToken;
+
     public BotInitializer(GroomingReminderBot bot) {
         this.bot = bot;
     }
@@ -28,7 +30,7 @@ public class BotInitializer {
         //This piece should not be rewritten into try-with-resources as it will close a telegram bot.
         try {
             TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
-            botsApplication.registerBot(BOT_TOKEN, bot);
+            botsApplication.registerBot(botToken, bot);
         } catch (TelegramApiException e) {
             logger.error("initBot - failed to initialize bot due to error", e);
         }
--- src/main/java/com/liubomur/configuration/BeansConfiguration.java
+++ src/main/java/com/liubomur/configuration/BeansConfiguration.java
@@ -1,18 +1,20 @@
 package com.liubomur.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
-import static com.liubomur.bot.constants.Constants.BOT_TOKEN;
-
 @Configuration
 public class BeansConfiguration {
 
+    @Value("${bot.token}")
+    private String botToken;
+
     @Bean
     TelegramClient telegramClient() {
-        return new OkHttpTelegramClient(BOT_TOKEN);
+        return new OkHttpTelegramClient(botToken);
     }
 
 }
